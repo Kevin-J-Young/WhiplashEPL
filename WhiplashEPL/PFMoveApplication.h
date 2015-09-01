5eec6ec4f438cc487317cc349290141440ba1ad4
Kevin <thanatic@gmail.com>
2015-08-31 17:06:03 -0700

    adding comment links for future reference

--- WhiplashEPL/PFMoveApplication.h
+++ WhiplashEPL/PFMoveApplication.h
@@ -5,6 +5,9 @@
 //  Created by Andy Kim at Potion Factory LLC on 9/17/09
 //
 //  The contents of this file are dedicated to the public domain.
+//
+// http://www.potionfactory.com/blog/2009/09/17/move-applications-folder
+// https://github.com/potionfactory/LetsMove/
 
 #ifdef __cplusplus
 extern "C" {
